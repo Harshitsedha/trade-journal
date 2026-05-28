794363fb90f8cea42ae4253e4233cb5bd292730c
Harshitsedha <sedhaharshitee@gmail.com>
2026-05-29 03:06:09 +0530

    fix: update all remaining prisma import paths

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from '../app/generated/prisma/client'
+import { PrismaClient, Prisma } from '../generated/prisma/client/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import 'dotenv/config'
 
